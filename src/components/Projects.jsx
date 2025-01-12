815fc95e2fff27c9547fe529caaa23d715119915
Kyle Dang <kylekd1@uci.edu>
2025-01-12 15:09:59 -0800

    Project technologies wrap on smaller screen widths

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,13 +16,15 @@ const Projects = () => {
                 <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1 }} className='w-full max-w-xl lg:w-3/4'>
                     <a href={project.repo} target='_blank' className='mb-2 font-semibold underline'>{project.title}</a>
                     <p className='mb-4 text-neutral-400'>{project.description}</p>
+                    <div className='flex flex-wrap'>
                     {project.technologies.map((tech, index) => {
                         return (
-                        <span key={index} className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>
+                        <span key={index} className='mr-2 mb-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>
                             {tech}
                         </span>
                         )
                     })}
+                    </div>
                 </motion.div>
             </div>
             )
